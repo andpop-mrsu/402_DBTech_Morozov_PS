41d94c28ae91a7534068e4347ccd2973f6583ff2
AngelMaks <petrmorozov85778@gmail.com>
2023-10-22 03:36:53 +0300

    Task02

---
+++ Task02/hangman/src/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MPrealy\hangman\Controller;
+
+use function cli\line;
+
+function startGame()
+{
+    line("Виселица");
+}
---
+++ Task02/hangman/src/View.php
@@ -0,0 +1,3 @@
+<?php
+
+namespace MPrealy\hangman\View;
